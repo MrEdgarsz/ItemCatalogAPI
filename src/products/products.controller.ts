37b18e6781ede3b0926760422126f24410c259a5
Doic <michalakradek2@gmail.com>
2023-01-13 18:42:22 +0100

    added constructor for ProductsService and created async func getProducts in ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Product } from './interfaces/product.interface';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
   @Get()
-  findAll(): string {
-    return 'All products returned';
+  async getProducts(): Promise<Product[]> {
+    return this.productsService.getAllProducts();
   }
 }
