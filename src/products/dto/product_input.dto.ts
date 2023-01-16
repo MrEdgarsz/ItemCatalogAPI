f93f285a119ebca30963ddfac4c2e8e56476aa0f
Daniel Obłąk <daniel.oblak99@gmail.com>
2023-01-16 22:28:39 +0100

    Added decorators in ProductInputDto

--- src/products/dto/product_input.dto.ts
+++ src/products/dto/product_input.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, MaxLength } from 'class-validator';
+
 import { Product } from '../models/product.model';
 
 export class ProductInputDto {
@@ -13,22 +15,29 @@ export class ProductInputDto {
     type: String,
     default: 'Droga Szamana. Etap 1: Początek',
   })
+  @IsString()
+  @MaxLength(80)
   name: string;
   @ApiProperty({
     description: 'Product image url source',
     type: String,
-    default: 'url without image',
+    default:
+      'https://www.mswordcoverpages.com/wp-content/uploads/2018/10/Book-cover-page-4-CRC.png',
   })
+  @IsUrl()
   image_src: string;
   @ApiProperty({
     description: 'Product category',
     type: String,
     default: 'Ksiazka',
   })
+  @IsString()
   category: string;
   @ApiProperty({
     description: 'Product description',
     type: String,
   })
+  @IsString()
+  @MaxLength(250)
   description: string;
 }
