f251e36c93b1d25533ae4e8b768129a3d26a357c
Daniel Obłąk <daniel.oblak99@gmail.com>
2023-01-20 16:38:29 +0100

    UserModel many to many relationship

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,7 +1,10 @@
+import { Product } from 'src/products/models/product.model';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
+  ManyToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -16,6 +19,10 @@ export class User {
 
   @Column({ select: false })
   password: string;
+
+  @ManyToMany(() => Product, { cascade: true })
+  @JoinTable()
+  favourites: Product[];
 
   @CreateDateColumn()
   created_at: Date;
